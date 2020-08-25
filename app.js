d1af0d1e58f8b968ccf8ab71bd7bff0918c598dd
Glen Park <glenpark00@gmail.com>
2020-08-25 16:41:06 -0700

    Change server to serve dist html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 let cors = require('cors');
 let bodyParser = require('body-parser');  
@@ -8,10 +9,15 @@ const listings = require('./routes/listing');
 
 app.use(cors())
 
-app.use(express.static(__dirname + '/dist'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
 
 app.use('/api/listings', listings);
 
