de8332245f08f00d42d35e7ba0326adf3ac54ca1
Glen Park <glenpark00@gmail.com>
2020-08-25 16:51:13 -0700

    Change server to serve dist html

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/oakland-airbnb/index.html'))
 })
 
 app.use('/api/listings', listings);
