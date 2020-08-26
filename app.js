d0a5eccc48d1b37df5332286043e6a1638d0e456
Glen Park <glenpark00@gmail.com>
2020-08-25 23:14:41 -0700

    Fix Api calls on heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/listings', listings);
 
 app.use(express.static(__dirname + '/frontend/dist'));
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/frontend/dist/index.html'))
 })
 
