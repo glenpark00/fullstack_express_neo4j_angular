74bd3b64bf84a1c0d9bab33086c61c36b482279a
Glen Park <glenpark00@gmail.com>
2020-08-25 21:10:44 -0700

    Remove slash

--- frontend/src/app/api-call.service.ts
+++ frontend/src/app/api-call.service.ts
@@ -5,7 +5,7 @@ import axios from "axios";
   providedIn: 'root'
 })
 export class ApiCallService {
-  private url = 'http://oakland-airbnb.herokuapp.com/';
+  private url = 'http://oakland-airbnb.herokuapp.com';
   // private url = 'http://localhost:5000';
 
   constructor() {
