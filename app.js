ab0bf9f331679db6a16b56fd8c5ee9604e47497a
Glen Park <glenpark00@gmail.com>
2020-08-25 19:14:57 -0700

    Attempt to proxy

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const path = require('path');
 const port = process.env.PORT || 5000;
-let cors = require('cors');
-let bodyParser = require('body-parser');  
+const bodyParser = require('body-parser');  
 
 const listings = require('./routes/listing');
 
-app.use(cors())
+app.use(cors({ credentials: true, origin: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- frontend/src/app/api-call.service.ts
+++ frontend/src/app/api-call.service.ts
@@ -1,32 +1,45 @@
 import { Injectable } from '@angular/core';
-import axios from 'axios';
+import axios from "axios";
+import { AxiosInstance } from "axios";
 import creds from '../../../config/credentials';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiCallService {
+  private axiosClient: AxiosInstance;
 
-  constructor() { }
+  constructor() {
+    this.axiosClient = axios.create({
+      timeout: 3000,
+      headers: {
+        "X-Initialized-At": Date.now().toString()
+      }
+    });
+  }
 
-  getListings = () => {
-    return axios({
-      method: 'get',
-      url: `${creds.url}/api/listings/`,
-      data: {
-      },
-      headers: { 'Access-Control-Allow-Origin': '*' }
-    }).then(res => {
-      return res.data.records.map(listing => listing._fields[0].properties);
-    })
+  // getListings = () => {
+  //   return axios({
+  //     method: 'get',
+  //     url: `${creds.url}/api/listings/`,
+  //   }).then(res => {
+  //     return res.data.records.map(listing => listing._fields[0].properties);
+  //   })
+  // }
+
+  getListings = async () => {
+    let axiosResponse = await this.axiosClient.request({
+      method: "get",
+      url: `${creds.url}/api/listings/`
+    });
+
+    return (axiosResponse.data);
   }
 
   searchListings = fragment => {
     return axios({
       method: 'get',
       url: `${creds.url}/api/listings/search/${fragment}`,
-      data: {
-      }
     }).then(res => {
       return res.data.records.map(listing => listing._fields[0].properties);
     })
